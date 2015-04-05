c9e7e33f684663dead566e957a8b0f88e8726282
Alexis Rico <me@sferadev.com>
2015-04-05 20:03:20 +0200

    vold: Fix cflags
    Change-Id: I218e366a3735eea27157f01c2d679a60930dbfc7

--- Android.mk
+++ Android.mk
@@ -1,6 +1,8 @@
 LOCAL_PATH:= $(call my-dir)
 
-common_cflags := -Werror -Wno-unused-parameter
+common_cflags := \
+	-Werror=format \
+	-Wno-unused-parameter
 
 common_src_files := \
 	VolumeManager.cpp \
